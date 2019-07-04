7f716e2de5360658942fa7df52112eb482084d51
Rakesh Tripathi <rakesht@chetu.com>
2019-07-04 23:39:28 +0530

    changes the speaker

--- public/wp-content/themes/joints/elements/acf-blocks/content-speakers-group.php
+++ public/wp-content/themes/joints/elements/acf-blocks/content-speakers-group.php
@@ -12,15 +12,16 @@ $imageUrl = $fields['speaker_image']['url'];
 console_log(__FILE__, $fields);
 ?>
 
-<div class="sub-hero-container mb-0 border-b10">
-    <div class="hero-sub-section text-center">
-        <div class="grid-container">
-             <div class="white margin-bottom-0 hero-sub-section-requirement-text speaker-image-section">
+<div class="sub-hero-container">
+	<div class="hero-sub-section text-center">
+		<div class="grid-container">
+			<div
+				class="white margin-bottom-0 hero-sub-section-requirement-text speaker-image-section">
               <?php if($imageUrl):?>
                <img src="<?php echo $imageUrl; ?>" alt="speaker-image">
                <?php endif;?>
             </div>
-            <div class="white hero-sub-section-text">
+			<div class="white hero-sub-section-text">
              <?php if($fields['term_title']):?>
                 <?php echo $fields['term_title']; ?>
             </div>
@@ -30,17 +31,20 @@ console_log(__FILE__, $fields);
                 <?php echo $fields['speaker_title']; ?>
             </div>
             <?php endif;?>
-            <p class="white margin-bottom-0 hero-sub-section-requirement-text">
+            <p
+				class="white margin-bottom-0 hero-sub-section-requirement-text">
             <?php if($fields['speaker_body']):?>
                 <?php echo $fields['speaker_body']; ?>
             </p>
             <?php endif;?>
-            <div class="white margin-bottom-0 hero-sub-section-requirement-text mt-20">
+            <div
+				class="white margin-bottom-0 hero-sub-section-requirement-text mt-20">
             <?php if($fields['enroll_url']):?>
-                <a href="<?php echo $fields['enroll_url']; ?>" class= "enroll-url enroll-btn"><strong>ENROLL FOR THIS TERM</strong></a>
-            </div>
+                <a href="<?php echo $fields['enroll_url']; ?>"
+					class="enroll-url enroll-btn"><strong>ENROLL FOR THIS TERM</strong></a>
+			</div>
            <?php endif;?>
         </div>
-    </div>
-    <div class="background-blue-container-triangle"></div>
+	</div>
+	<div class="background-blue-container-triangle"></div>
 </div>
