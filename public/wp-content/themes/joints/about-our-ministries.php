bb5e57e64909bd796d0d7cb4c850b23d89d6c3c8
Rakesh Tripathi <rakesht@chetu.com>
2019-07-10 22:19:27 +0530

    remove white space

--- public/wp-content/themes/joints/about-our-ministries.php
+++ public/wp-content/themes/joints/about-our-ministries.php
@@ -8,7 +8,7 @@ get_header(); ?>
 	
 	<div class="content">
 	
-		<div class="inner-content grid-x grid-margin-x grid-padding-x">
+		<div class="inner-content grid-x grid-margin-x">
 	
 		    <main class="main small-12 large-12 medium-12 cell" role="main">
 				
