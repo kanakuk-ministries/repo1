1e2bfdd5b9dea0d54923fd7ec310f00bab2a6823
Rakesh Tripathi <rakesht@chetu.com>
2019-07-10 15:35:44 +0530

    resloved the issue for left content for saftey page

--- public/wp-content/themes/joints/about-our-ministries.php
+++ public/wp-content/themes/joints/about-our-ministries.php
@@ -10,7 +10,7 @@ get_header(); ?>
 	
 		<div class="inner-content grid-x grid-margin-x grid-padding-x">
 	
-		    <main class="main small-12 large-8 medium-8 cell" role="main">
+		    <main class="main small-12 large-12 medium-12 cell" role="main">
 				
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		    
@@ -30,7 +30,7 @@ get_header(); ?>
 			    					
 			</main> <!-- end #main -->
 
-		    <?php get_sidebar(); ?>
+		    <?php //get_sidebar(); ?>
 		    
 		</div> <!-- end #inner-content -->
 
