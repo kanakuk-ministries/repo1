4db2e3a623c844a8b434aeb321c016a4a84dd29b
Rakesh Tripathi <rakesht@chetu.com>
2019-07-11 12:55:42 +0530

    remove the unwanted..

--- public/wp-content/themes/joints/elements/acf-blocks/content-about-card.php
+++ public/wp-content/themes/joints/elements/acf-blocks/content-about-card.php
@@ -25,7 +25,7 @@ console_log(__FILE__, $fields);
                  &bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;
                 </p>
 				<a class="link-btn" href="#myModal1" data-toggle="modal">Read More</a>
-   <div id="myModal1" class="modal fade" tabindex="-1">
+  			 <div id="myModal1" class="modal fade" tabindex="-1">
 					<div class="modal-dialog modal-lg aboutInfoModal">
 						<div class="modal-content">
 							<div class="modal-header">
