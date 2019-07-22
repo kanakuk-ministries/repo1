4f5d4873ef6b0bbeb7385ced4ed2edeb797ef4c8
Rakesh Tripathi <rakesht@chetu.com>
2019-07-22 17:25:02 +0530

    added the description popup

--- public/wp-content/themes/joints/about-all-activities.php
+++ public/wp-content/themes/joints/about-all-activities.php
@@ -1,19 +1,18 @@
 <?php /* Template Name: About >> All Activities */ ?>
 <?php 
 /**
- * The template for displaying an about page
- */
+* The template for displaying an about page
+*/
 $about_pages = array('Our Story', 'Teams', 'Our Ministries', 'All Activities', 'Ceremonies', 'FAQs');
 $kamp_types = get_kamp_types();
-$team_activites = get_fields();
 $kamps = get_kamps();
 get_header(); ?>
-	
-	<div class="content">
-	
-		<div class="inner-content grid-x grid-margin-x">
-	
-		<main class="main small-12 medium-12 large-12 cell tabs-view about-page-main" role="main">
+    
+    <div class="content">
+    
+        <div class="inner-content grid-x grid-margin-x">
+    
+        <main class="main small-12 medium-12 large-12 cell tabs-view about-page-main" role="main">
             <div class="hero-tabs-container about-page margin-bottom-60">
                 <ul class="tabs hero-tabs about-page-tabs" id="about-page-tabs">
                     <?php foreach ($about_pages as $key => $value): ?>
@@ -24,116 +23,135 @@ get_header(); ?>
                     </li>
                     <?php endforeach;?>
                 </ul>
-						</div>
-						
-						
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		    
-					<?php 
-					$post = get_post(); 
+                        </div>
+                        
+                        
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            
+                    <?php 
+                    $post = get_post(); 
 
-					if ( has_blocks( $post->post_content ) ) {
-						$blocks = gutenberg_parse_blocks( $post->post_content );
-					
-						foreach ($blocks as $block) {
-							echo gutenberg_render_block($block);
-							
-							
-						}
-					}
-					?>
-			    
-					<?php endwhile; endif; ?>	
+                    if ( has_blocks( $post->post_content ) ) {
+                        $blocks = gutenberg_parse_blocks( $post->post_content );
+                    
+                        foreach ($blocks as $block) {
+                            echo gutenberg_render_block($block);
+                        }
+                    }
+                    ?>
                 
-					<div class="grid-container">
-						<div class="accordion-image-grid-container margin-top-50">
-							<h1 class="dark-blue"><?php echo $team_activites['team_title']; ?></h1>
-							<p class="dark-blue margin-bottom-30"><?php echo $team_activites['team_description']; ?></p>
-							<ul class="accordion list-grid-accordion-container" data-accordion data-multi-expand="true" data-allow-all-closed="true" data-deep-link="true">
-							<?php foreach ($kamp_types as $key => $value): ?>
-								<li class="accordion-item <?php if ($key === 0) { echo 'is-active'; }; ?>" data-accordion-item>
-									<!-- Accordion title -->
-									<a href="#" class="accordion-title list-grid-accordion-title-container" style="background-color: <?php echo $value['color']; ?>">
-										<div class="accordion-title-icon-container"><i class="icon large white"></i></div>
-										<div class="accordion-title-text-container">
-											<h4 class="white list-grid-accordion-title-header"><?php echo $value['title']; ?> Kamp</h4>
-											<i class="icon white large icon-<?php echo $value['icon_name']; ?>"></i>
-										</div>
-									</a>
+                    <?php endwhile; endif; ?>   
 
-									<!-- Accordion content: use `is-active` state class to start in open position. -->
-									<ul class="accordion-content" data-tab-content>
-										<?php foreach ($kamps as $kamp_key => $kamp): ?>
-											<?php if ($kamp['kamp_type']->post_title === $value['title']) : ?>
-												<div class="list-grid-section-container">
-													<div class="flex-container align-center list-grid-section-sub-header-container">
-														<h4 class="margin-bottom-0 margin-top-15 kamp-<?php echo $value['icon_name']; ?>"><?php echo $kamp['kamp_title']; ?></h4>
-														<div class="kamp-<?php echo $value['icon_name']; ?> list-grid-section-sub-header"></div>
-													</div>
-													<?php if (isset($kamp['activities']) && reset($kamp['activities'])): ?>
-														<ul class="list-grid-container activity-grid">
-															<?php
-																$i = 0;
-																foreach ((reset($kamp['activities'])) as $activity_key => $activity):
-																	if ($i === 4)
-																		$i = 0;
-																	$i++;
-															?>
-																<?php
-																	$activity_id = array($activity->ID);
-																	$this_activity_array = get_activities($activity_id);
-																	$this_activity = reset($this_activity_array);
-																?>
-																
-				<div id="myModal<?php echo $activity_id[0]?>"
-					class="modal fade popout-about-card">
-					<div class="modal-dialog modal-lg aboutInfoModal">
-						<div class="modal-content">
-							<div class="modal-header">
-								<button class="close" type="button" data-dismiss="modal">
-									<i class="fa fa-times" aria-hidden="true"></i>
-								</button>
-								<h4 class="modal-title card-section"><?php echo $this_activity['activity_name']; ?></h4>
-							</div>
-							<div class="modal-body customModalBody"><?php echo $this_activity['activity_description']; ?></div>
-						</div>
-						<!-- /.modal-content -->
-					</div>
-					<!-- /.modal-dialog -->
-				</div>
-				<!-- /.modal -->
-																<div class="list-grid-item block-<?php echo $i; ?>">
-																	<img src="<?php echo $this_activity['activity_image']['url']; ?>">
-																	<div class="list-grid-item-overlays">
-																		<h6 class="secondary bold white text-center titlecase margin-bottom-20"><?php echo $this_activity['activity_name']; ?></h6>
-																		<h6 class="secondary bold white text-center titlecase margin-bottom-20"><a class="link-btn" href="#myModal<?php echo $activity_id[0];?>"
-					data-toggle="modal">Read More</a></h6>
-																		<?php //if(true): ?>
-<!-- 																			<button class="button expanded white hollow">Watch Video</button> -->
-																		<?php //endif; ?>
-																	</div>
-																</div>
-															<?php endforeach; ?>
-														</ul>
-													<?php endif; ?>
-												</div>
-											<?php endif; ?>
-										<?php endforeach;?>
-									</ul>
-								</li>
-							<?php endforeach;?>
-						</ul>
-						</div>
-						
-					</div>
-									
-			    					
-			</main> <!-- end #main -->
+                    <div class="grid-container">
+                        <div class="accordion-image-grid-container margin-top-50">
+                            <h1 class="dark-blue">Activities</h1>
+                            <p class="dark-blue margin-bottom-30">Something about all of the activities offered accross the kamps.</p>
+                            <ul class="accordion list-grid-accordion-container" data-accordion data-multi-expand="true" data-allow-all-closed="true" data-deep-link="true">
+                            <?php foreach ($kamp_types as $key => $value): ?>
+                                <li class="accordion-item <?php if ($key === 0) { echo 'is-active'; }; ?>" data-accordion-item>
+                                    <!-- Accordion title -->
+                                    <a href="#" class="accordion-title list-grid-accordion-title-container" style="background-color: <?php echo $value['color']; ?>">
+                                        <div class="accordion-title-icon-container"><i class="icon large white"></i></div>
+                                        <div class="accordion-title-text-container">
+                                            <h4 class="white list-grid-accordion-title-header"><?php echo $value['title']; ?> Kamp</h4>
+                                            <i class="icon white large icon-<?php echo $value['icon_name']; ?>"></i>
+                                        </div>
+                                    </a>
 
-		    <?php get_sidebar(); ?>
-		    
-		</div> <!-- end #inner-content -->
+                                    <!-- Accordion content: use `is-active` state class to start in open position. -->
+                                    <ul class="accordion-content" data-tab-content>
+                                        <?php foreach ($kamps as $kamp_key => $kamp): ?>
+                                            <?php if ($kamp['kamp_type']->post_title === $value['title']) : ?>
+                                                <div class="list-grid-section-container">
+                                                    <div class="flex-container align-center list-grid-section-sub-header-container">
+                                                        <h4 class="margin-bottom-0 margin-top-15 kamp-<?php echo $value['icon_name']; ?>"><?php echo $kamp['kamp_title']; ?></h4>
+                                                        <div class="kamp-<?php echo $value['icon_name']; ?> list-grid-section-sub-header"></div>
+                                                    </div>
+                                                    <?php if (isset($kamp['activities']) && reset($kamp['activities'])): ?>
+                                                        <ul class="list-grid-container activity-grid all-activity">
+                                                            <?php
+                                                                $i = 0;
+                                                                foreach ((reset($kamp['activities'])) as $activity_key => $activity):
+                                                                    if ($i === 4)
+                                                                        $i = 0;
+                                                                    $i++;
+                                                            ?>
+                                                                <?php
+                                                                    $activity_id = array($activity->ID);
+                                                                    $this_activity_array = get_activities($activity_id);
+                                                                    $this_activity = reset($this_activity_array);
+                                                                ?>
+                                                                <div class="list-grid-item block-<?php echo $i; ?>"  data-toggle="modal" data-target="#about-all-activities">
+                                                                <!-- <a href="#" class="Update"> </a>        -->
+                                                                <img src="<?php echo $this_activity['activity_image']['url']; ?>">
+                                                                        <div class="list-grid-item-overlays">
+                                                                        <h6 class="secondary bold white text-center titlecase margin-bottom-20"><?php echo $this_activity['activity_name']; ?></h6>
+                                                                        <?php //if(true): ?>
+<!--                                                                            <button class="button expanded white hollow">Watch Video</button> -->
+                                                                        <?php //endif; ?>
+                                                                    </div>
+                                                                    
+                                                                    <script>                                                                        
+                                                                        $(function () {
+                                                                            $("a[class='Update']").click(function () {
+                                                                                $("#MyPopup").modal("show");
+                                                                                return false;
+                                                                            });
+                                                                        });
+                                                                    </script>
+                                                                    
+                                                                </div>
+                                                            <?php endforeach; ?>
+                                                        </ul>
+                                                    <?php endif; ?>
+                                                </div>
+                                            <?php endif; ?>
+                                        <?php endforeach;?>
+                                    </ul>
+                                </li>
+                            <?php endforeach;?>
+                        </ul>
+                        </div>
+                        
+                    </div>
+                                    
+                                    
+            </main> <!-- end #main -->
 
-	</div> <!-- end #content -->
+            <div id="about-all-activities" class="modal fade popout-about-card image-popup">
+                <div class="modal-dialog modal-lg aboutInfoModal">
+                    <div class="modal-content">
+                            <button class="close" type="button" data-dismiss="modal"><i class="fa fa-times" aria-hidden="true"></i></button>
+                        <img src="<?php echo $this_activity['activity_image']['url']; ?>">
+                    </div>
+                    <!-- /.modal-content -->
+                </div>
+                <!-- /.modal-dialog -->
+            </div>
 
+            <?php get_sidebar(); ?>
+            
+        </div> <!-- end #inner-content -->
+
+    </div> <!-- end #content -->
+    
+    <!-- <div id="MyPopup" class="modal fade in" role="dialog" aria-hidden="false" style="display: block;">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">
+                    ×</button>
+                <h4 class="modal-title">
+                    Greetings
+                </h4>
+            </div>
+            <div class="modal-body">
+                Welcome to ASPSnippets.com
+            </div>
+            <div class="modal-footer">
+                <input type="button" id="btnClosePopup" value="Close" class="btn btn-danger" data-dismiss="modal">
+            </div>
+        </div>
+    </div>
+</div> -->
 <?php get_footer(); ?>
